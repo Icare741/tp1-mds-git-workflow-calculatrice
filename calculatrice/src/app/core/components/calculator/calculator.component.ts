77e4d132828af84b7297f0b9dab05aa2ac6bb07d
Icare741 <enzo.lebuhotel@hotmail.fr>
2023-07-03 12:56:17 +0200

    feat(export-csv): Implement CSV export functionality

--- calculatrice/src/app/core/components/calculator/calculator.component.ts
+++ calculatrice/src/app/core/components/calculator/calculator.component.ts
@@ -44,5 +44,17 @@ export class CalculatorComponent {
  
   }
   
+  exportToCSV() {
+    const csvContent = this.calculationHistory.join('\n');
+    const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+    const link = document.createElement('a');
+    const url = URL.createObjectURL(blob);
+    link.setAttribute('href', url);
+    link.setAttribute('download', 'calcul_history.csv');
+    link.style.visibility = 'hidden';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
   
 }
